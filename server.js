05a962be039b7b8df9ce1b591962602c3cea4f9e
Luke Bosse <lukebosse@u.boisestate.edu>
2018-04-04 16:31:40 -0600

    fixed heroku error H20

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, 
+app.listen(process.env.PORT || 3000, 
   () => console.log('Example app listening on port 3000!'));
